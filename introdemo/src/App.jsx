53683e552bd198d94a204c9b0fad23d92ecc33d9
Gurshwin <gurshwinprince@outlook.com>
2025-07-07 14:55:39 +0200

    Removed nested Git repo

---
+++ introdemo/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';          
+
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+export default App
+
+
